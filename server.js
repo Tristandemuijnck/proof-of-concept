b7853ff07f0bff54ac01f021e3a562b1c9eaf9a4
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-06-08 16:04:16 +0200

    Added basic GET with token

--- server.js
+++ server.js
@@ -1,17 +1,43 @@
 import express from "express"
+import * as dotenv from "dotenv"
 
+// Load .env file
+dotenv.config()
+
+/* -------------------------------------------------------------------------- */
+/*                                Server setup                                */
+/* -------------------------------------------------------------------------- */
 const server = express()
 
 server.set("view engine", "ejs")
 server.set("views", "./views")
 server.set("port", process.env.PORT || 8000)
-
 server.use(express.static('public'))
 
 server.listen(server.get("port"), () => {
 	console.log(`Application started on http://localhost:${server.get("port")}`)
 })
 
-server.get("/", (req, res) => {
-	res.render("index")
+/* -------------------------------------------------------------------------- */
+/*                                Server Routes                               */
+/* -------------------------------------------------------------------------- */
+server.get("/", async (req, res) => {
+	const info = await dataFetch("https://api.werktijden.nl/2/employees")
+	res.render("index", {info})
 })
+
+/* -------------------------------------------------------------------------- */
+/*                                API Functions                               */
+/* -------------------------------------------------------------------------- */
+const options = {
+	method: "GET",
+	headers: {
+		Authorization: `Bearer ${process.env.API_KEY}`,
+	}
+}
+async function dataFetch(url) {
+	const data = await fetch(url, options)
+		.then((response) => response.json())
+		.catch((error) => error);
+	return data;
+}
